ccc561a4499b4b494cd82c56b731650f4971b992
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-30 11:39:28 -0300

    Created Footer and Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Footer from './component/Footer';
+import Header from './component/header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/component/header/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+const Header = () => (
+    <header className="header-container">
+            <h2 className="header-title">DB POKEMON</h2>
+    </header>
+)
+
+export default Header;
